9a2e918d07e50bda263595078d3b2cc4f9d557fb
hwrdyen <hwrdpvt@gmail.com>
2023-12-20 16:47:45 -0500

    normalizeURL and tests

---
+++ crawl.test.js
@@ -0,0 +1,31 @@
+const { normalizeURL } = require("./crawl.js");
+const { test, expect } = require("@jest/globals");
+
+test("normalizeURL strip protocol", () => {
+  const input = "https://blog.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip trailing slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL captitals", () => {
+  // URL constructor already did it for us because it knows that URLs care case sensitive
+  const input = "https://BLOG.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip http", () => {
+  const input = "http://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
